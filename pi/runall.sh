bdaf78afbd9744cc17a7c49ba43cdee442eacc52
Douglas G. Moore <doug@dglmoore.com>
2018-04-04 04:50:24 +0000

    Rearrange the π sources

--- pi/runall.sh
+++ pi/runall.sh
@@ -7,32 +7,42 @@ else
     trials=$1;
 fi
 
-echo "Computing π in C:"
-time sh pi-c.sh $trials
+pushd C >/dev/null
+    echo "Computing π in C:"
+    time sh pi.sh $trials
+popd >/dev/null
 
-echo -e "\nComputing π in C++:"
-time sh pi-cpp.sh $trials
+pushd "C++" >/dev/null
+    echo -e "\nComputing π in C++:"
+    time sh pi.sh $trials
+popd >/dev/null
 
-echo -e "\nComputing π (for-loop) in Python 2.7:"
-time python36 pi.py $trials
+pushd Python >/dev/null
+    echo -e "\nComputing π (for-loop) in Python 2.7:"
+    time python36 pi.py $trials
 
-echo -e "\nComputing π (for-loop) in Python 3.6:"
-time python36 pi.py $trials
+    echo -e "\nComputing π (for-loop) in Python 3.6:"
+    time python36 pi.py $trials
 
-echo -e "\nComputing π (vectorized) in Python 2.7:"
-time python36 pi-numpy.py $trials
+    echo -e "\nComputing π (vectorized) in Python 2.7:"
+    time python36 pi-numpy.py $trials
 
-echo -e "\nComputing π (vectorized) in Python 3.6:"
-time python36 pi-numpy.py $trials
+    echo -e "\nComputing π (vectorized) in Python 3.6:"
+    time python36 pi-numpy.py $trials
+popd >/dev/null
 
-echo -e "\nComputing π (for-loop) in Julia 0.6.2:"
-time julia pi.jl $trials
+pushd Julia >/dev/null
+    echo -e "\nComputing π (for-loop) in Julia 0.6.2:"
+    time julia pi.jl $trials
 
-echo -e "\nComputing π (vectorized) in Julia 0.6.2:"
-time julia pi-vec.jl $trials
+    echo -e "\nComputing π (vectorized) in Julia 0.6.2:"
+    time julia pi-vec.jl $trials
+popd >/dev/null
 
-echo -e "\nComputing π (for-loop) in R:"
-time Rscript pi.r $trials
+pushd R >/dev/null
+    echo -e "\nComputing π (for-loop) in R:"
+    time Rscript pi.r $trials
 
-echo -e "\nComputing π (vectorized) in R:"
-time Rscript pi-vec.r $trials
+    echo -e "\nComputing π (vectorized) in R:"
+    time Rscript pi-vec.r $trials
+popd >/dev/null
