ede62d557861c9330fece654f4f4c5cbceb55634
Douglas G. Moore <doug@dglmoore.com>
2018-04-07 23:28:21 +0000

    Implement tests for Playground class

--- game-theory/python/playground/test/test_playground.py
+++ game-theory/python/playground/test/test_playground.py
@@ -3,16 +3,41 @@
 # license that can be found in the LICENSE file.
 from unittest import TestCase
 
-import playground as play
+from playground import *
 
 class TestPlayground(TestCase):
     """
     The `TestPlayground` class provides a suite of tests to validate the
     `Playground` class.
     """
-
     def test_canary(self):
         """
         Ensure the shaft isn't filling with carbon dioxide.
         """
         self.assertEqual(1 + 2, 3)
+
+    def test_invalid_game(self):
+        """
+        Constructing a playground with an invalid game raises an `ValueError`.
+        """
+        with self.assertRaises(ValueError):
+            Playground([])
+
+        with self.assertRaises(ValueError):
+            Playground([1,2])
+
+        with self.assertRaises(ValueError):
+            Playground([[1,2]])
+
+        with self.assertRaises(ValueError):
+            Playground([[1,2],[3,4],[5,6]])
+
+        with self.assertRaises(ValueError):
+            Playground([[1,2,3],[4,5,6]])
+
+    def test_valid_playground(self):
+        """
+        Ensure that we can properly construct a playground
+        """
+        p = Playground(PrisonersDilemma(0.25,0.75))
+        self.assertTrue(np.array_equal(p.game.payoff, [[0.75,0.0],[1.0,0.25]]))
