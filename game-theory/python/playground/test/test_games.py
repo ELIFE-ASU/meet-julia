c7566e771f34a66e4a6d3d4122eb60c2cea239fc
Douglas G. Moore <doug@dglmoore.com>
2018-04-07 18:50:19 +0000

    Create init tests for TwoPlayerGame class

--- game-theory/python/playground/test/test_games.py
+++ game-theory/python/playground/test/test_games.py
@@ -15,3 +15,35 @@ class TestGames(TestCase):
         Is there too much carbon monoxide in the air?
         """
         self.assertEqual(1 + 2, 3)
+
+    def test_two_player_invalid_payoff(self):
+        """
+        Ensure that `TwoPlayerGame` raises a `ValueError` when the provided
+        payoff matrix is invalid (i.e. not a 2x2 matrix).
+        """
+        with self.assertRaises(ValueError):
+            play.TwoPlayerGame([])
+
+        with self.assertRaises(ValueError):
+            play.TwoPlayerGame([1,2])
+
+        with self.assertRaises(ValueError):
+            play.TwoPlayerGame([[1,2]])
+
+        with self.assertRaises(ValueError):
+            play.TwoPlayerGame([[1,2],[3,4],[5,6]])
+
+        with self.assertRaises(ValueError):
+            play.TwoPlayerGame([[1,2,3],[4,5]])
+
+        with self.assertRaises(ValueError):
+            play.TwoPlayerGame([[1,2,3],[4,5,6]])
+
+    def test_two_player_init(self):
+        """
+        Ensure that the payoff matrix is properly assigned on construction of a
+        `TwoPlayerGame`.
+        """
+        payoff = [[0.1, 0.2], [0.3, 0.4]]
+        game = play.TwoPlayerGame(payoff)
+        self.assertTrue(np.array_equal(payoff, game.payoff))
