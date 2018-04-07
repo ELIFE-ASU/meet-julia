dc39fbcc16e1a024d0563dae72fb10dba4f0fee1
Douglas G. Moore <doug@dglmoore.com>
2018-04-07 19:36:12 +0000

    Add documentation for TwoPlayerGame

--- game-theory/python/playground/docs/source/index.rst
+++ game-theory/python/playground/docs/source/index.rst
@@ -1,16 +1,14 @@
-.. Playground documentation master file, created by
-   sphinx-quickstart on Sat Apr  7 18:27:54 2018.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
-Welcome to Playground's documentation!
-======================================
+Playground
+==========
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
+.. automodule:: playground.games
 
+    .. autoclass:: TwoPlayerGame
+        :members: __init__
 
 Indices and tables
 ==================
--- game-theory/python/playground/playground/games.py
+++ game-theory/python/playground/playground/games.py
@@ -1,21 +1,40 @@
 # Copyright 2018 ELIFE. All rights reserved.
 # Use of this source code is governed by a MIT
 # license that can be found in the LICENSE file.
+"""
+.. testsetup::
+
+    from playground.games import *
+"""
 import numpy as np
 
 class TwoPlayerGame(object):
     """
     The `TwoPlayerGame` type represents a two-player game using a
-    :math:`2\times2` payoff matrix. Aside from the shape, the payoff matrix is
+    :math:`2\\times2` payoff matrix. Aside from the shape, the payoff matrix is
     unconstrained.
 
-    The `TwoPlayerGame` provides the root of a hierarchy of two player games.
+    The `TwoPlayerGame` provides the root of a hierarchy of two-player games.
     Derived classes will impose constraints upon the payoff to ensure they
     conform to specific game structures.
     """
     def __init__(self, payoff):
         """
         Construct a `TwoPlayerGame` from a payoff matrix.
+
+        .. doctest::
+
+            >>> g = TwoPlayerGame([[0,1],[1,0]])
+            >>> g.payoff
+            array([[0., 1.],
+                   [1., 0.]])
+            >>> TwoPlayerGame([])
+            Traceback (most recent call last):
+            ...
+            ValueError: payoff must have shape (2,2); got (0,)
+
+        :param payoff: the payoff matrix
+        :raises ValueError: if `payoff` is not :math:`2\\times2`
         """
         self.payoff = np.asarray(payoff, dtype=np.float64)
         shape = self.payoff.shape
