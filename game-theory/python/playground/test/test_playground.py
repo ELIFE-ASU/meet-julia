26e37a77762e217f7402b7857e752ba8701b5bb6
Douglas G. Moore <doug@dglmoore.com>
2018-04-08 01:25:01 +0000

    Implement tests of Playground.update

--- game-theory/python/playground/test/test_playground.py
+++ game-theory/python/playground/test/test_playground.py
@@ -10,6 +10,12 @@ class TestPlayground(TestCase):
     The `TestPlayground` class provides a suite of tests to validate the
     `Playground` class.
     """
+    def setUp(self):
+        """
+        Setup the test class with a random number generator
+        """
+        self.rng = np.random.RandomState(2018)
+
     def test_canary(self):
         """
         Ensure the shaft isn't filling with carbon dioxide.
@@ -52,3 +58,31 @@ class TestPlayground(TestCase):
         p.payoff([0,1,0,1], payoff)
         self.assertTrue(np.array_equal(payoff,
             [[0.75,1.25], [1.5,2.], [0.75,1.5], [0.75,1.]]))
+
+    def test_deterministic_update(self):
+        """
+        Ensure that we can synchronously update the agents' strategies using
+        a deterministic rule.
+        """
+        game = HawkDove(0.25, 0.75)
+
+        graph = nx.Graph()
+        graph.add_nodes_from(range(4))
+        graph.add_edges_from([(0,1), (0,2), (1,2), (2,3)])
+
+        # Deterministic update
+        rule = lambda p0, p1 : 0.0 if p1 <= p0 else 1.0
+
+        p = Playground(game, graph, rule)
+
+        state = [0,0,0,0]
+        next_state = p.update(state, rng=self.rng)
+        self.assertTrue(np.array_equal(next_state, [1,1,1,1]))
+
+        p.update(next_state, next_state, rng=self.rng)
+        self.assertTrue(np.array_equal(next_state, [0,0,0,0]))
+
+        state = next_state
+        next_state = np.zeros(len(next_state), dtype=np.int)
+        p.update(state, next_state, rng=self.rng)
+        self.assertTrue(np.array_equal(next_state, [1,1,1,1]))
