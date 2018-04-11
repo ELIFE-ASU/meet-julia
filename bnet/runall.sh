02ffa3a70ebcac84b6a665c99aa20993a8191991
Douglas G. Moore <doug@dglmoore.com>
2018-04-11 06:49:29 +0000

    Fix messages in bnet

--- bnet/runall.sh
+++ bnet/runall.sh
@@ -1,7 +1,7 @@
 #!/bin/env sh
 
 if [[ $# -lt 2 ]]; then
-    (>&2 echo -e "ERROR: must provide a data file\n");
+    (>&2 echo -e "ERROR: must provide data files\n");
     exit 1;
 else
     weights=$1;
@@ -9,15 +9,15 @@ else
 fi
 
 pushd "C++" >/dev/null
-    echo "Running n-body simulation in C++:"
+    echo "Finding attractors in C++:"
     time sh attractors.sh ../$weights ../$thresholds
 popd >/dev/null
 
-echo -e "\nRunning n-body simulation in Python 2.7:"
+echo -e "\nFinding attractors in Python 2.7:"
 time python27 attractors.py $weights $thresholds
 
-echo -e "\nRunning n-body simulation in Python 3.6:"
+echo -e "\nFinding attractors in Python 3.6:"
 time python36 attractors.py $weights $thresholds
 
-echo -e "\nRunning n-body simulation in Julia 0.6.2:"
+echo -e "\nFinding attractors in Julia 0.6.2:"
 time julia attractors.jl $weights $thresholds
