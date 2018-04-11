4e7efe885da3b935c61d7fab5c6e7fc057077be4
Douglas G. Moore <doug@dglmoore.com>
2018-04-11 19:22:51 +0000

    Add package install script

---
+++ packages.jl
@@ -0,0 +1,31 @@
+Pkg.update()
+
+# Package Development
+Pkg.add("PkgDev")
+
+# Jupyter Kernel
+Pkg.add("IJulia")
+
+# ALL OF THE PLOTTING!!!!
+Pkg.add("PyPlot")
+Pkg.add("Gadfly")
+Pkg.add("GR")
+Pkg.add("Hiccup")
+Pkg.add("Plots")
+Pkg.add("StatPlots")
+
+# Graphics File Formats
+Pkg.add("Cairo")
+
+# Networks
+Pkg.add("LightGraphs")
+
+# Calling Python
+Pkg.add("PyCall")
+
+# Data Analysis
+Pkg.add("DataFrames")
+Pkg.add("CSV")
+Pkg.add("RDatasets")
+Pkg.add("StatsBase")
+Pkg.add("HypothesisTests")
